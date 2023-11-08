d4f87b4f1dd9493d27c7d5894e2400e24844a5e6
renebaumann3000 Baumann <rene.baumann84@outlook.com>
2023-11-08 15:01:52 +0000

    added profil image + static files function

--- gistgroup/blog/models.py
+++ gistgroup/blog/models.py
@@ -17,6 +17,8 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="profile/images/")
+    socialmedia_url = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return str(self.user)
