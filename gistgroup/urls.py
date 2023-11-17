9547fbf77a76eee833a4a91bf27d4d01e885e4f9
renebaumann3000 Baumann <rene.baumann84@outlook.com>
2023-11-17 17:45:28 +0000

    url check

--- gistgroup/urls.py
+++ gistgroup/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
 
